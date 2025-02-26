4a0f0ffe284d5eebf45f13421b2f3bdcae8f5a9a
Aaron Spence <aaronspence31@gmail.com>
2025-02-26 16:47:38 -0500

    fix fe ts errors

--- web-app/src/components/InfoBottom.tsx
+++ web-app/src/components/InfoBottom.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useRef } from "react";
 import * as d3 from "d3";
 
-const InfoBottom = ({ tripDistance }) => {
+const InfoBottom = ({ tripDistance }: { tripDistance: number }) => {
   const containerRef = useRef(null);
 
   useEffect(() => {
